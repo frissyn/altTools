9e414eb00d86b4a501e00160490e118e35d5f0e4
Noodlez76 <62401019+Noodlez76@users.noreply.github.com>
2020-03-19 16:25:26 -0400

    Update and rename altTools.py to __init__.py

--- altTools/__init__.py
+++ altTools/__init__.py
@@ -1,5 +1,9 @@
-import sys
-from time import sleep
+try:
+    import sys
+    from time import sleep
+    from collections import Counter
+except ImportError as e:
+    print('ImportError occured when initiating altTools:\n', str(e))
 
 def write(values, pause=0.06, newline=True):
     for char in str(values):
@@ -30,21 +34,18 @@ def encrypt(value, shift):
         else: result += chr((ord(char) + shift - 97) % 26 + 97)
     return result
 
+def is_anagram(val1, val2):
+    return Counter(str(val1)) == Counter(str(val2))
+
+def del_duplicates(list1):
+    if type(list1) is not list:
+        raise ValueError("'list1' paramter must be a list value!")
+    result = []
+    for num in list1:
+        if num not in result: result.append(num)
+    if len(result) >= 1: return result
+    else: return list1
+
 def about():
     x = "altTools is Python Package created to contain a collection of\nunrealated and useful functions for a coder's convienience when needed.\nSome tools are pretty miscellaneous extras that do things like Ceasar\nCipher encryption and some tools have more useful applications like\nrandomly shuffling two iterable objects in the same order (not yet\nimplemented). A lot of these functions and tools already exist and are\nknown in the Python community elsewhere, but now you can find most of them\nin one place!"
     return x
-
-def info():
-    functions = ['write(value, pause=0.06, newline=True)', 'clr_input(prompt)', 'encrypt(value, shift)']
-    descriptions = [
-        'Prints out values to the stream and pauses shortly between each value to create a typewriter effect.',
-        'Works like a regular input statement, but clears the prompt and input value after a newline key is pressed.',
-        'Returns a Ceasar Cipher encrypted version of a string corresponding to the shift prameter value.'
-    ]
-    table = 'Description of altTools functions:\n\n'
-
-    if len(functions) == len(descriptions):
-        for i in range(len(functions)):
-            table += str(functions[i]) + ' : ' + str(descriptions[i]) + '\n'
-            return table
-    else: return "find more information at 'https://pypi.org/project/altTools/'"
